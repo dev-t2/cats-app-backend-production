223931c527ec9a2bc8977debe87ed542864269c3
t2 <oo_ri@naver.com>
2022-02-05 22:53:27 +0900

    07_set Complete

--- 07_set/src/app.ts
+++ 07_set/src/app.ts
@@ -4,6 +4,8 @@ const a: AB = 'A';
 
 console.log(a);
 
+console.log();
+
 const ab: AB = Math.random() < 0.5 ? 'A' : 'B';
 
 console.log(ab);
@@ -15,3 +17,21 @@ type AB12 = 'A' | 'B' | 12;
 const ab12: AB12 = ab;
 
 console.log(ab12);
+
+console.log();
+
+interface Person {
+  name: string;
+}
+
+interface PersonSpan extends Person {
+  birth: Date;
+  death?: Date;
+}
+
+const personSpan: PersonSpan = {
+  name: 'TypeScript',
+  birth: new Date('1989/04/01'),
+};
+
+console.log(personSpan);
