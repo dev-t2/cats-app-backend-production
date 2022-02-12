7e9709ec101e842646017090ad9ae889d86fcc4a
t2 <oo_ri@naver.com>
2022-02-12 11:11:09 +0900

    10_wrapper Complete

---
+++ 10_wrapper/src/app.ts
@@ -0,0 +1,10 @@
+const getStringLength = (text: String) => {
+  return text.length;
+};
+
+console.log(getStringLength('hello'));
+
+console.log();
+
+console.log(typeof BigInt(123456789));
+console.log(typeof Symbol('symbol'));
