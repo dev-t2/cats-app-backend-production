f2187fc3dc35681813de7eb01cb7b590e9eab5be
dev-t2 <oo_ri@naver.com>
2022-10-18 16:58:46 +0900

    Logger Middleware Morgan Style Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/common/middlewares/logger.middleware.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+interface IRequest extends Request {
+  user?: { id: number };
+}
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(req: IRequest, res: Response, next: NextFunction) {
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+      const contentLength = res.getHeader('content-length') || 0;
+
+      const responseTime = Date.now() - startTime;
+
+      if (process.env.NODE_ENV !== 'production') {
+        this.logger.log(
+          `${req.method} ${req.originalUrl} ${res.statusCode} ${responseTime} ms - ${contentLength}`,
+        );
+      } else {
+        const ip = req.ip || req.socket.remoteAddress;
+        const userId = req.user?.id || 0;
+        const referrer = req.header('Referer') || req.header('Referrer') || '-';
+        const userAgent = req.header('user-agent');
+
+        this.logger.log(
+          `${ip} - ${userId} "${req.method} ${req.originalUrl} HTTP/${req.httpVersion}" ${res.statusCode} ${responseTime} ms - ${contentLength} "${referrer}" "${userAgent}"`,
+        );
+      }
+    });
+
+    next();
+  }
+}
