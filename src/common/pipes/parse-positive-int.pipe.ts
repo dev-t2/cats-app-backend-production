caaf31bf449d36733c35b3871aa23d41982744c6
dev-t2 <oo_ri@naver.com>
2022-10-19 17:51:03 +0900

    Parse Positive Integer Pipe Error Message Update

--- src/common/pipes/parse-positive-int.pipe.ts
+++ src/common/pipes/parse-positive-int.pipe.ts
@@ -6,7 +6,9 @@ export class ParsePositiveIntPipe implements PipeTransform<string, number> {
     const parsedInt = parseInt(value, 10);
 
     if (isNaN(parsedInt) || parsedInt < 1) {
-      throw new BadRequestException('Validation failed (positive integer string is expected)');
+      throw new BadRequestException(
+        `Validation failed (positive integer string is expected): ${value}`,
+      );
     }
 
     return parsedInt;
