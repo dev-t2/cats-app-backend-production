e3a3ed7ab904760d9bab7df6476f410a667039c6
dev-t2 <oo_ri@naver.com>
2022-10-20 10:26:43 +0900

    Chats Gateway Code Refactoring

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -4,30 +4,21 @@ import {
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chats' })
-export class ChatsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  private readonly logger = new Logger('Chats');
-
-  constructor() {
-    this.logger.log('Constructor');
-  }
-
-  afterInit() {
-    this.logger.log('After Init');
-  }
+export class ChatsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  private readonly logger = new Logger(ChatsGateway.name);
 
   handleConnection(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`Connected ${socket.nsp.name}: ${socket.id}`);
+    this.logger.log(`Connected ${socket.nsp.name} ${socket.id}`);
   }
 
   handleDisconnect(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`Disconnected ${socket.nsp.name}: ${socket.id}`);
+    this.logger.log(`Disconnected ${socket.nsp.name} ${socket.id}`);
   }
 
   @SubscribeMessage('createUser')
